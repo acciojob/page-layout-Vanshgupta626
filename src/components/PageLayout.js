a38fbb7d758ef1ab517d684fd6b0c1603c50e1eb
vansh gupta <vanshg6532@gmail.com>
2026-04-09 06:57:56 +0530

    pagelayout

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const PageLayout = ({ header, footer, children }) => {
   return (
-    <div>
+    <>
       {header}
       {children}
       {footer}
-    </div>
+    </>
   );
 };
 
